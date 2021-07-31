72d32fba9ed5bf15d4c57dabc1b3e56324cf89e2
y-mak <makado.y2527@gmail.com>
2021-07-31 14:29:44 +0900

    メッセージ送信機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_31_014621) do
+ActiveRecord::Schema.define(version: 2021_07_31_051444) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
